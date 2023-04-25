2e47ff04fbaf152819f6c617dd02b538d7162d54
Maria Eduarda Sales de Castro <castro.madusales@gmail.com>
2023-04-25 10:57:46 -0300

    update

--- js/script.js
+++ js/script.js
@@ -11,8 +11,23 @@ function ajustaTamanhoPalcoJogo() {
 
 ajustaTamanhoPalcoJogo()
 
-var posiçaoX = Math.random() *largura
-var posiçaoY = Math.random() * altura
+function posiçaoRandomica() {
+
+var posiçaoX = Math.floor(Math.random() * largura) - 90
+var posiçaoY = Math.floor(Math.random() * altura) - 90
+
+posiçaoX = posiçaoX < 0 ? 0 : posiçaoX
+posiçaoY = posiçaoY < 0 ? 0 : posiçaoY
 
 console.log(posiçaoX, posiçaoY)
 
+//criar o elemento html
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosquito.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posiçaoX + 'px'
+mosquito.style.top = posiçaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
+}
