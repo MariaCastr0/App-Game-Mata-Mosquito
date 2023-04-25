4f6762148cc07c3c26fb217ffb179c23207b7b57
Maria Eduarda Sales de Castro <castro.madusales@gmail.com>
2023-04-25 11:21:18 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,10 +24,25 @@ console.log(posiçaoX, posiçaoY)
 //criar o elemento html
 var mosquito = document.createElement('img')
 mosquito.src = 'imagens/mosquito.png'
-mosquito.className = 'mosquito1'
+mosquito.className = tamanhoAleatorio()
 mosquito.style.left = posiçaoX + 'px'
 mosquito.style.top = posiçaoY + 'px'
 mosquito.style.position = 'absolute'
 
 document.body.appendChild(mosquito)
+
+
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
 }
